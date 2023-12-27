5d638e8b914b2d246b752bb413c73a8152207678
Emanuel <zerauleuname@gmail.com>
2023-12-26 20:28:57 -0600

    nav implementado y modal con titulos

--- pages/obra.js
+++ pages/obra.js
@@ -19,7 +19,6 @@ import i12 from "/public/images/masa/tequiero.jpg";
 import i13 from "/public/images/masa/tracksuit.jpg";
 import i14 from "/public/images/mascaras-2019/2.jpg";
 import i15 from "/public/images/mascaras-2019/3.jpg";
-import i16 from "/public/images/mascaras-2019/4.jpg";
 import i17 from "/public/images/mascaras-2019/5.jpg";
 import i18 from "/public/images/mascaras-2019/6.jpg";
 import i19 from "/public/images/mascaras-2019/7.jpg";
@@ -45,83 +44,185 @@ import i38 from "/public/images/turno-vespertino/2.jpg";
 import i39 from "/public/images/turno-vespertino/3.jpg";
 
 const imgMap = [
-  {src: i1, text: "obra asi muy padre"},
-  {src: i2, text: ""},
-  {src: i3, text: ""},
-  {src: i4, text: ""},
-  {src: i5, text: ""},
-  {src: i6, text: ""},
-  {src: i7, text: ""},
-  {src: i8, text: ""},
-  {src: i9, text: ""},
-  {src: i10, text: ""},
-  {src: i11, text: ""},
-  {src: i12, text: ""},
-  {src: i13, text: ""},
-  {src: i14, text: ""},
-  {src: i15, text: ""},
-  {src: i16, text: ""},
-  {src: i17, text: ""},
-  {src: i18, text: ""},
-  {src: i19, text: ""},
-  {src: i20, text: ""},
-  {src: i21, text: ""},
-  {src: i23, text: ""},
-  {src: i24, text: ""},
-  {src: i25, text: ""},
-  {src: i26, text: ""},
-  {src: i27, text: ""},
-  {src: i28, text: ""},
-  {src: i29, text: ""},
-  {src: i30, text: ""},
-  {src: i31, text: ""},
-  {src: i32, text: ""},
-  {src: i33, text: ""},
-  {src: i34, text: ""},
-  {src: i35, text: ""},
-  {src: i36, text: ""},
-  {src: i37, text: ""},
-  {src: i38, text: ""},
-  {src: i39, text: ""},
+  {src: i2, text: "Sonar"},
+  {src: i1, text: "Sonar (detalle)"},
+  {src: i3, text: "Trinidad"},
+  {src: i5, text: "Triste erexión"},
+  {src: i4, text: "Triste erexión (detalle)"},
+  {src: i7, text: "Caso Nash"},
+  {src: i6, text: "Caso Nash (detalle)"},
+  {src: i8, text: "Sacramentos, masa galeria, 2023"},
+  {src: i9, text: "Sacramentos, masa galeria, 2023"},
+  {src: i10, text: "Segundo sacramento"},
+  {src: i11, text: "Servicio"},
+  {src: i12, text: "Te quiero"},
+  {src: i13, text: "Tracksuit lovers"},
+  {src: i14, text: "Máscara de burro 1"},
+  {src: i15, text: "Máscara de burro 2"},
+  {src: i17, text: "Máscara de árbol"},
+  {src: i18, text: "Máscara de winipú"},
+  {src: i19, text: "Máscara de rana"},
+  {src: i20, text: "Máscara de teatro 2"},
+  {src: i21, text: "Máscara de teatro 1"},
+  {src: i22, text: "Máscara de humano"},
+  {src: i23, text: "Máscara de abulón 2"},
+  {src: i24, text: "Máscara de abulón 1 "},
+  {src: i25, text: "Máscara de abulón 1"},
+  {src: i26, text: "Máscara de pescao"},
+  {src: i27, text: "Máscara de pájaro carpintero"},
+  {src: i28, text: "Máscara de chango"},
+  {src: i30, text: "Cuemanco 1"},
+  {src: i29, text: "Cuemanco 2 "},
+  {src: i31, text: "Máscaras de piedra, Son de aquí, 2023"},
+  {src: i32, text: "Máscara de piedra 1"},
+  {src: i36, text: "Máscara de piedra 2"},
+  {src: i35, text: "Máscara de piedra 3"},
+  {src: i33, text: "Máscara de piedra 5"},
+  {src: i34, text: "Máscara de piedra 4"},
+  {src: i37, text: "Turno Vespertino 1"},
+  {src: i39, text: "Turno Vespertino 2"},
+  {src: i38, text: "Turno Vespertino 3"},
 ];
 
+const carboncillos = [
+  {src: i2, text: "Sonar"},
+  {src: i1, text: "Sonar (detalle)"},
+  {src: i3, text: "Trinidad"},
+  {src: i5, text: "Triste erexión"},
+  {src: i4, text: "Triste erexión (detalle)"},
+];
 
+const masc2019 = [
+  {src: i14, text: "Máscara de burro 1"},
+  {src: i15, text: "Máscara de burro 2"},
+  {src: i17, text: "Máscara de árbol"},
+  {src: i18, text: "Máscara de winipú"},
+  {src: i19, text: "Máscara de rana"},
+  {src: i20, text: "Máscara de teatro 2"},
+  {src: i21, text: "Máscara de teatro 1"},
+  {src: i22, text: "Máscara de humano"},
+  {src: i23, text: "Máscara de abulón 2"},
+  {src: i24, text: "Máscara de abulón 1 "},
+  {src: i25, text: "Máscara de abulón 1"},
+  {src: i26, text: "Máscara de pescao"},
+  {src: i27, text: "Máscara de pájaro carpintero"},
+  {src: i28, text: "Máscara de chango"},
+  {src: i30, text: "Cuemanco 1"},
+  {src: i29, text: "Cuemanco 2 "},
+];
+
+const mascPiedra = [
+  {src: i31, text: "Máscaras de piedra, Son de aquí, 2023"},
+  {src: i32, text: "Máscara de piedra 1"},
+  {src: i36, text: "Máscara de piedra 2"},
+  {src: i35, text: "Máscara de piedra 3"},
+  {src: i33, text: "Máscara de piedra 5"},
+  {src: i34, text: "Máscara de piedra 4"},
+];
+
+const sacramentos = [
+  {src: i7, text: "Caso Nash"},
+  {src: i6, text: "Caso Nash (detalle)"},
+  {src: i8, text: "Sacramentos, masa galeria, 2023"},
+  {src: i9, text: "Sacramentos, masa galeria, 2023"},
+  {src: i10, text: "Segundo sacramento"},
+  {src: i11, text: "Servicio"},
+  {src: i12, text: "Te quiero"},
+  {src: i13, text: "Tracksuit lovers"},
+];
+
+const vespertino = [
+  {src: i37, text: "Turno Vespertino 1"},
+  {src: i39, text: "Turno Vespertino 2"},
+  {src: i38, text: "Turno Vespertino 3"},
+];
 
 const Work = () => {
     const windowSize = useWindowSize();
     const [show, setShow] = useState(false);
     const handleShow = (val) => () => setShow(val);
+    const [active, setActive] = useState("gral")
+
   return (
 
     <Layout>
         <div className={styles.obraMainDiv}>
           <div className={styles.categoryNavDiv}>
-            <div className={styles.categoryButton}><p>carboncillos</p></div>
-            <div className={styles.categoryButton}><p>máscaras 2019</p></div>
-            <div className={styles.categoryButton}><p>máscaras de piedra</p></div>
-            <div className={styles.categoryButton}><p>sacramentos (masa galería)</p></div>
-            <div className={styles.categoryButton}><p>turno vespertino</p></div>
+            <div className={styles.categoryButton} onClick={()=> {setActive("gral")}}><p>todo</p></div>
+            <div className={styles.categoryButton} onClick={()=> {setActive("carboncillos")}}><p>carboncillos</p></div>
+            <div className={styles.categoryButton} onClick={()=> {setActive("masc2019")}}><p>máscaras 2019</p></div>
+            <div className={styles.categoryButton} onClick={()=> {setActive("mascPiedra")}}><p>máscaras de piedra</p></div>
+            <div className={styles.categoryButton} onClick={()=> {setActive("sacramentos")}}><p>sacramentos</p></div>
+            <div className={styles.categoryButton} onClick={()=> {setActive("vespertino")}}><p>turno vespertino</p></div>
           </div>
           <div className={styles.obraContainer}>
+          {active === "gral" && (<>
             {imgMap.map((img, i) => (<img 
                       onClick={handleShow(i)}
                       src={(img.src).src} 
                       className={styles.obraGridItem}
                   />)
               )}
+          </>)}
+          {active === "carboncillos" && (<>
+            {carboncillos.map((img, i) => (<img 
+                      onClick={handleShow(i)}
+                      src={(img.src).src} 
+                      className={styles.obraGridItem}
+                  />)
+              )}
+          </>)}
+          {active === "masc2019" && (<>
+            {masc2019.map((img, i) => (<img 
+                      onClick={handleShow(i)}
+                      src={(img.src).src} 
+                      className={styles.obraGridItem}
+                  />)
+              )}
+          </>)}
+          {active === "mascPiedra" && (<>
+            {mascPiedra.map((img, i) => (<img 
+                      onClick={handleShow(i)}
+                      src={(img.src).src} 
+                      className={styles.obraGridItem}
+                  />)
+              )}
+          </>)}
+          {active === "sacramentos" && (<>
+            {sacramentos.map((img, i) => (<img 
+                      onClick={handleShow(i)}
+                      src={(img.src).src} 
+                      className={styles.obraGridItem}
+                  />)
+              )}
+          </>)}
+          {active === "vespertino" && (<>
+            {vespertino.map((img, i) => (<img 
+                      onClick={handleShow(i)}
+                      src={(img.src).src} 
+                      className={styles.obraGridItem}
+                  />)
+              )}
+          </>)}
           </div>
-          <Modal centered size="xs" show={show !== false} onHide={handleShow(false)}>
-              <img src={imgMap[show]?.src.src} className="modalImage"/>
+          {windowSize.width > 2000 ? (
+            <Modal centered size="lg" show={show !== false} onHide={handleShow(false)}>
+              <img src={imgMap[show]?.src.src} className={styles.modalImage}/>
               <div className={styles.modalBottomDiv}>
                 <p className={styles.obraInfo}>{imgMap[show]?.text}</p>
               </div>
-          </Modal>
+            </Modal>
+          ):(
+            <Modal centered size="xs" show={show !== false} onHide={handleShow(false)}>
+              <img src={imgMap[show]?.src.src} className={styles.modalImage}/>
+              <div className={styles.modalBottomDiv}>
+                <p className={styles.obraInfo}>{imgMap[show]?.text}</p>
+              </div>
+            </Modal>
+          )}
+
         </div>
-        {/* {windowSize.width > 752 ? (
-            <div></div>
-        ):(
-            <div></div>
-        )} */}
+
     </Layout>
   )
 }
